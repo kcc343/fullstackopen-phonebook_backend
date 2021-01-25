533eb6d89f4e009c1a815e74c3df4374c2b4119f
kcc343 <kcc343@uw.edu>
2021-01-25 11:23:50 -0800

    ex 3.11 added production build

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const app = express()
 app.use(cors())
 app.use(express.json())
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :data'))
+app.use(express.static('build'))
 
 morgan.token('data', function(req, res) {
   return JSON.stringify(req.body);
